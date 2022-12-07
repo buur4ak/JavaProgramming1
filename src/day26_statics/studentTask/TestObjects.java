69648982cb755c7b3cc8f9052a7397c3df5c9362
buur4ak <114306566+buur4ak@users.noreply.github.com>
2022-12-06 22:08:20 -0500

    added home page

---
+++ src/day26_statics/studentTask/TestObjects.java
@@ -0,0 +1,22 @@
+package day26_statics.studentTask;
+public class TestObjects {
+
+    public static void main(String[] args) {
+
+        Student student1 = new Student("Yusuf", 21, 'M', "A16");
+        Student student2 = new Student("Glenio", 24, 'M', "B34");
+        Student student3 = new Student("Sumeyye", 22, 'F', "C56");
+        Student student4 = new Student("Mehmet", 25, 'M', "D43");
+        Student student5 = new Student("Sebastian", 23, 'M', "E27");
+
+    }
+
+}
+
+
+/*
+3. Create a class named TestObjects
+	            3.1 Create 5 Student objects
+	            3.2 Create a StudentsGroup object
+	            3.3 Add all the student objets to the StudentsGroup object' students list
+ */
