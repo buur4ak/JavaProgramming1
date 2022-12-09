3a57569708866a5079555e296084008788d174b5
buur4ak <114306566+buur4ak@users.noreply.github.com>
2022-12-08 23:07:43 -0500

    added home page

---
+++ src/day26_statics/TestCarObjects.java
@@ -0,0 +1,27 @@
+package day26_statics;
+
+public class TestCarObjects {
+
+    public static void main(String[] args) {
+
+     Car car1 = new Car("BMW");
+        System.out.println(car1);
+
+     Car car2 = new Car("Audi","Q4");
+        System.out.println(car2);
+
+     Car car3 = new Car("Mazda","CX9","Black");
+        System.out.println(car3);
+
+     Car car4 = new Car("Toyota","Camry","White",2021);
+        System.out.println(car4);
+
+     Car car5 =new Car("Honda","Pilot","Blue",2020,25000);
+        System.out.println(car5);
+
+
+
+
+
+    }
+}
