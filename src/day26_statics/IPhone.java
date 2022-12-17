97085eee4281c8dd99d75ff27939f1a508d15055
buur4ak <114306566+buur4ak@users.noreply.github.com>
2022-12-17 14:07:32 -0500

    added home page

--- src/day26_statics/IPhone.java
+++ src/day26_statics/IPhone.java
@@ -1,18 +1,17 @@
 package day26_statics;
 
 public class IPhone {
+    public static String brand = "Apple"; //static: "Apple" is the brand of all the iphone objects
 
-    public static String brand = "Apple"; // static: Apple is the brand of all the IPhone object's
-
-    public String model; // instance(anlIk dEGISKEN) different models for IPhone objects
+    public String model; //instance: different models for iphone objects
 
     public String color;
 
     public double price;
 
-    public static String Os = "IOS"; // static: operating system is same from all the IPhone objects
+    public static String OS = "iOS"; // static: operating system is same fro all the iphone objects
 
-    public static String madeIn = "China"; // static:
+    public static String madeIn = "China";
 
     public static boolean hasBattery = true;
 
@@ -20,35 +19,40 @@ public class IPhone {
 
     public static boolean isExpensiveToFix = true;
 
-
     public IPhone(String model, String color, double price) {
         this.model = model;
         this.color = color;
         this.price = price;
-
     }
 
 
 
-    public void printPhoneInfo() {
+    /*
+    public static void printPhoneInfo(){ // static only accepts statics
+        System.out.println("Model: " + model);
+        System.out.println("Color: " + color);
+    }
+    */
+
+
+    public void printPhoneInfo() { // instance methods accepts both static and none static
         System.out.println("Brand: " + brand);
         System.out.println("Model: " + model);
         System.out.println("Color: " + color);
         System.out.println("Price: " + price);
-
     }
 
 
-    public static void printPhoneOperatingSystem() {
-        System.out.println("Operating System: " + Os);
+    public static void printOperatingSystem(){
+        System.out.println("Operating System: " + OS);
     }
+
+
+
 }
-
-
-
-
 
 /*
 Attributes:
-Brand,model,color,price,OS,madeIn,hasBattery,isTouchScreen,isExpensiveToFix
+    brand, model, color, price, OS, madeIn, hasBattery, isTouchScreen, isExpensiveToFix
  */
+
