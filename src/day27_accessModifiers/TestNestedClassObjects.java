be70ece240ef29a9295c9f60b97d76a2ef643577
buur4ak <114306566+buur4ak@users.noreply.github.com>
2022-12-25 15:09:01 -0500

    added home page

--- src/day27_accessModifiers/TestNestedClassObjects.java
+++ src/day27_accessModifiers/TestNestedClassObjects.java
@@ -5,9 +5,9 @@ public class TestNestedClassObjects {
     public static void main(String[] args) {
 
         Car obj1 = new Car();
-        obj1.new CarEngine();
+        Car.CarEngine obj2 = obj1.new CarEngine();
 
-        new Car.StaticInnerClass();
+        Car.StaticInnerClass obj3 = new Car.StaticInnerClass();
 
 
 
