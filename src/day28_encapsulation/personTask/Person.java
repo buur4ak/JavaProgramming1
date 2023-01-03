25bc9b8ab7cbb4b393e38142f0ccddf7511833a5
buur4ak <114306566+buur4ak@users.noreply.github.com>
2023-01-02 22:26:10 -0500

    added home page

--- src/day28_encapsulation/personTask/Person.java
+++ src/day28_encapsulation/personTask/Person.java
@@ -39,6 +39,35 @@ public class Person {
         this(name, age, gender);
         this.language = language;
     }
+
+    static{
+        planet = "Earth";
+        isHuman = true;
+        hasNose = true;
+        numberOfWings = 0;
+        numberOfHead = 1;
+    }
+
+    public static void printPlanetName(){
+        System.out.println("Planet name is: "+planet);
+    }
+
+    public void eat(String food){
+        System.out.println(name+" is eating "+food);
+    }
+    public void drink(String drink){
+        System.out.println(name+" is drinking "+drink);
+    }
+
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", gender=" + gender +
+                ", language='" + language + '\'' +
+                ", planet='" + planet + '\'' +
+                '}';
+    }
 }
 
 /*
