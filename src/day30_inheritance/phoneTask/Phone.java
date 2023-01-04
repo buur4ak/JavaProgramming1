0266310499ed636afa699cc14e5b3826f03d8ea6
buur4ak <114306566+buur4ak@users.noreply.github.com>
2023-01-03 19:22:38 -0500

    added home page

--- src/day30_inheritance/phoneTask/Phone.java
+++ src/day30_inheritance/phoneTask/Phone.java
@@ -4,9 +4,16 @@ import java.util.Arrays;
 
 public class Phone {
 
-    private String brand, model, size,color;
+    private String brand, model, size, color;
     private double price;
 
+    public Phone(String brand, String model, String size, String color, double price) {
+        setBrand(brand);
+        setModel(model);
+        setSize(size);
+        setColor(color);
+        setPrice(price);
+    }
 
     public String getBrand() {
         return brand;
@@ -37,9 +44,8 @@ public class Phone {
     }
 
     public void setPrice(double price) {
-
-        if(price <= 0){
-            System.err.println("Invalid Price: "+price);
+        if( price <= 0){
+            System.err.println("Invalid price: " + price);
             System.exit(1);
         }
         this.price = price;
@@ -50,35 +56,49 @@ public class Phone {
     }
 
     public void setColor(String color) {
-        String[] colors ={"Black", "White", "Silver", "Gold", "Pink"};
-
-        if( Arrays.asList(colors).contains(color) ){
+        String[] colors = {"Black", "White", "Silver", "Gold", "Pink", "Blue", "Red", "Gray"};
+        if( Arrays.asList(colors).contains(color) ) {
             this.color = color;
         }else{
-            System.err.println("Invalid Color: "+color+" color of the can only be: "+ Arrays.toString(colors));
+            System.err.println("Invalid color: "+color+"\n color of the can only be : "+ Arrays.toString(colors));
             System.exit(1);
         }
 
     }
+
+    public void call(long phoneNumber){
+        System.out.println(getModel()+" is calling "+phoneNumber);
+    }
+
+    public void text(long phoneNumber){
+        System.out.println(getModel()+" is texting to "+phoneNumber);
+    }
+
+    public String toString() {
+        return  getClass().getSimpleName() + "{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", size='" + size + '\'' +
+                ", color='" + color + '\'' +
+                ", price=" + price +
+                '}';
+    }
+
+
 }
 
 /*
-Warmup tasks:
-	1. Create a named Phone:
+1. Create a named Phone:
 				Variables:
 					brand, model, size, price, color
-
 				Encapsulate all the fields
 					Conditions:
 						1. price can not be zero or negative
 						2. color can only be set to:
 								{"Black", "White", "Silver", "Gold", "Pink"}
-
 				Add a constructor that can set all the fields
-
 				Methods:
 					call(long phoneNumber)
 					text(long phoneNumber)
 					toString()
-
  */
