189617a79e9c083d53db363ffe8cbc4ed313aca8
buur4ak <114306566+buur4ak@users.noreply.github.com>
2023-01-03 19:23:56 -0500

    added home page

---
+++ src/day30_inheritance/phoneTask/Nokia.java
@@ -0,0 +1,11 @@
+package day30_inheritance.phoneTask;
+
+public class Nokia extends Phone{
+
+
+
+
+    public Nokia(String brand, String model, String size, String color, double price) {
+        super(brand, model, size, color, price);
+    }
+}
