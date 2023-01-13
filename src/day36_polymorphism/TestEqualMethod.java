dabfdbe85c580f53a92f4e217488bc21ce04c1ce
buur4ak <114306566+buur4ak@users.noreply.github.com>
2023-01-12 22:11:15 -0500

    added home page

--- src/day36_polymorphism/TestEqualMethod.java
+++ src/day36_polymorphism/TestEqualMethod.java
@@ -13,6 +13,20 @@ public class TestEqualMethod {
         System.out.println(circle1.equals(circle3)); // false
 
 
+        System.out.println("-----------------------------------------------------------------");
+
+        IPhone iphone1 = new IPhone("Apple","Iphone 12","Medium","Black",900);
+
+        IPhone iphone2 = new IPhone("Apple","Iphone 12","Medium","White",900);
+
+
+        System.out.println( iphone1.equals(iphone2));
+
+        System.out.println("-----------------------------------------------------------------");
+
+
+
+
     }
 
 }
