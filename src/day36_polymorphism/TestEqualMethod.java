8a764a981a896ec165954dc2567ad1f9644fda68
buur4ak <114306566+buur4ak@users.noreply.github.com>
2023-01-12 21:31:19 -0500

    added home page

---
+++ src/day36_polymorphism/TestEqualMethod.java
@@ -0,0 +1,18 @@
+package day36_polymorphism;
+
+public class TestEqualMethod {
+
+    public static void main(String[] args) {
+
+        Circle circle1 = new Circle(5);
+        Circle circle2 = new Circle(5);
+        Circle circle3 = new Circle(15);
+
+        System.out.println( circle1 == circle2); // false
+        System.out.println(circle1.equals(circle2)); //true
+        System.out.println(circle1.equals(circle3)); // false
+
+
+    }
+
+}
