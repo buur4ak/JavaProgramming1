1871b3713f41c978e4ab3295e6b8719a08ff50e2
buur4ak <114306566+buur4ak@users.noreply.github.com>
2023-01-23 21:40:34 -0500

    added home page

--- src/day39_collections/ListDemo.java
+++ src/day39_collections/ListDemo.java
@@ -1,7 +1,64 @@
 package day39_collections;
 
+import java.util.*;
+
 public class ListDemo {
 
-    
+    public static void main(String[] args) {
+
+
+        ArrayList<Integer> arrayList = new ArrayList<>(); // Array Based Class ===> get() is faster
+        arrayList.add(100);
+        arrayList.add(200);
+        arrayList.add(300);
+
+        arrayList.remove(0);
+
+        arrayList.get(0);
+
+        LinkedList<Integer> linkedList = new LinkedList<>(); // Node based class (Doubly linked List) ===> add(), addAll(), remove(), removeAll(), removeIf(), retainAll() are faster
+
+        linkedList.add(100);
+        linkedList.add(200);
+        linkedList.add(300);
+
+        linkedList.remove(0);
+
+        linkedList.get(0);
+
+
+        System.out.println("------------------------------------------------------------");
+
+
+        Vector<Integer> vector = new Vector<>();
+
+        vector.add(100);
+        vector.add(200);
+        vector.add(300);
+
+        vector.get(0);
+
+
+        System.out.println("------------------------------------------------------------");
+
+        Stack<Integer> stack = new Stack<>();
+        stack.add(100);
+        stack.add(200);
+        stack.add(300);
+
+        System.out.println(stack);
+
+        int lastElement = stack.pop(); // remove the last element in arraylist
+
+        System.out.println(stack);
+
+        int secondLastElement = stack.pop(); // remove the last element in arraylist again
+
+        System.out.println(stack);
+
+
+
+
+    }
 
 }
